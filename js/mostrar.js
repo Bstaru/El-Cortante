cdaabd289a51a47c9418dfa48dd3f885a928fcfd
Natalie Conde <black_star_big@hotmail.com>
2017-04-24 00:15:22 -0500

    pantalla de noticia individual con comentarios y JS para mostrar el comentatio

--- js/mostrar.js
+++ js/mostrar.js
@@ -32,23 +32,44 @@ function ValidarLogin(){
       $(".flechita1").css ("display","none");
       $(".flechita2").css ("display","none");
       
-    var correo= $(".correo").val();
-    var contra = $(".contra").val();
-    
-    if((correo!="") && (contra != "")){         
-    $(".formLog").submit();
-    }
-    else{
-        if(correo==""){
-            $(".correo").css ("border-color","#fc6a6a");
-            $(".flechita1").css ("display","inline");
-        }
-         if(contra==""){
-            $(".contra").css ("border-color","#fc6a6a");
-             $(".flechita2").css ("display","inline");
-        }
-    }
+      var correo= $(".correo").val();
+      var contra = $(".contra").val();
+      
+      if((correo!="") && (contra != "")){         
+      $(".formLog").submit();
+      }
+      else{
+          if(correo==""){
+              $(".correo").css ("border-color","#fc6a6a");
+              $(".flechita1").css ("display","inline");
+          }
+           if(contra==""){
+              $(".contra").css ("border-color","#fc6a6a");
+               $(".flechita2").css ("display","inline");
+          }
+      }
 }
 $(document).ready(function(){
     $(".oksesion").click(ValidarLogin);
+});
+
+
+$(document).ready(function(){ 
+ 
+    $('#btnCo').on('click',function() {
+
+        $('#comentario2').slideDown();
+    });
+ 
+});
+
+$(document).ready(function(){ 
+ 
+    $('#ComentarNo').on('click',function() {
+
+      $("#txtCo2").val('');
+
+        $('#comentario2').slideUp();
+    });
+ 
 });
